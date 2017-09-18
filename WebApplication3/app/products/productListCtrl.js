dc8dd46fa0dbc14f6acbfc4cdf1f639d515c0dfd
Abhishek Kumar <abhishekism0010@gmail.com>
2017-09-18 17:32:31 +0530

    Controller file is added and necessary settings were done

---
+++ WebApplication3/app/products/productListCtrl.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("productListCtrl",
+                        ProductListCtrl);
+    function ProductListCtrl() {
+        var vm = this; //When "controller as" syntax is used the model is defined in this variable. 
+         
+    };
+});
