3e98ebe0ee5bc84337810ac491356eee8ab588fd
Abhishek Kumar <abhishekism0010@gmail.com>
2017-09-19 18:16:27 +0530

    Added productDetailsCtrl.js controller containing an IIFE and a sample hardcoded product.

---
+++ WebApplication3/app/products/productDetailsCtrl.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("ProductDetailsCtrl",
+            ProductDetailsCtrl)
+    function ProductDetails() {
+        var vm = this;
+
+        vm.product = {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2010",
+            "description": "15 gallon capacity rolling garden cart",
+            "cost": 20.00,
+            "price": 32.99,
+            "category": "garden",
+            "tags": ["barrow", "cart", "wheelbarrow"],
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        };
+
+        vm.title = "Product Details : " + vm.product.productName;
+        if (vm.product.tags) {
+            vm.product.tags = vm.product.tags.toString();
+        }
+
+
+    }
+}());
--- WebApplication3/app/products/productListCtrl.js
+++ WebApplication3/app/products/productListCtrl.js
@@ -9,6 +9,7 @@
     function ProductListCtrl(productResource) {
         var vm = this; //When "controller as" syntax is used the model is defined in this variable. 
 
+        //this function is directly refering to the function written in productResource.js
         productResource.query(function (data) {
             vm.products = data;
         });
