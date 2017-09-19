68451400b4411edf6ecd24df41a867ee2dbdbc8b
Abhishek Kumar <abhishekism0010@gmail.com>
2017-09-19 11:27:18 +0530

    Created two new JavaScript Files for adding a service to fetch resources from API url. Added dependencied in app.js and index.html

---
+++ WebApplication3/common/services/productResource.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    angular
+        .module("common.services")
+        .factory("productResource",
+                ["$resource",
+            productResource]);
+
+    function productResource($resource) {
+        return $resource("/api/products/:productId")
+    }
+        
+}());
