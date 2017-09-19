228ae74cf74d391e44ffdb19e7be851760a1835c
Abhishek Kumar <abhishekism0010@gmail.com>
2017-09-19 16:21:16 +0530

    Added  $stateProvider in app.js inside app.config Also added $urlRouterProvider for default route setting

--- WebApplication3/app/app.js
+++ WebApplication3/app/app.js
@@ -5,4 +5,20 @@
         ["common.services",
             "ui.router",
             "productResourceMock"]);
+
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+                function ($stateProvider, $urlRouterProvider) {
+                    $urlRouterProvider.otherwise("/products");
+
+                    $stateProvider
+                        .state("productList", {
+                            url: "/products",
+                            templateUrl: "app/products/productListView.html",
+                            controller: "ProductListCtrl as vm"
+                    })
+            
+        }]
+       
+    );
 } ());
