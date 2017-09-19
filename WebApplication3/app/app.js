fab19e6151fd1b95c0d12008128d146b3a4ea5e5
Abhishek Kumar <abhishekism0010@gmail.com>
2017-09-19 16:38:01 +0530

    Added all states in app.js Added a navbar in index.html using bootstrap

--- WebApplication3/app/app.js
+++ WebApplication3/app/app.js
@@ -9,14 +9,31 @@
     app.config(["$stateProvider",
                 "$urlRouterProvider",
                 function ($stateProvider, $urlRouterProvider) {
-                    $urlRouterProvider.otherwise("/products");
+                    $urlRouterProvider.otherwise("/");
 
                     $stateProvider
+                        .state("home", {
+                            url: "/",
+                            templateUrl: "app/welcomeview.html"
+                        })
+
                         .state("productList", {
                             url: "/products",
                             templateUrl: "app/products/productListView.html",
                             controller: "ProductListCtrl as vm"
-                    })
+                        })
+
+                        .state("productEdit", {
+                            url: "/products/edit/:productId",
+                            templateUrl: "app/products/productEditView.html",
+                            controller: "ProductEditCtrl as vm"
+                        })
+
+                        .state("productDetails", {
+                            url: "/products/details/:productId",
+                            templateUrl: "app/products/productDetailsView.html",
+                            controller: "ProductDetailsCtrl as vm"
+                        })
             
         }]
        
